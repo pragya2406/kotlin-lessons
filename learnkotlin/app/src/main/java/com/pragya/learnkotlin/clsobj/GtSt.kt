f8719054e00c42a80c3acab118f2cae66b65a89d
pragya2406 <pragyat1920@gmail.com>
2021-09-29 17:04:27 +0530

    uses of visibility

--- learnkotlin/app/src/main/java/com/pragya/learnkotlin/clsobj/GtSt.kt
+++ learnkotlin/app/src/main/java/com/pragya/learnkotlin/clsobj/GtSt.kt
@@ -18,4 +18,21 @@ fun main(){
     val gt1=GtSt(10,56) //get() is called
     gt1.x=10 //set(value) is called
     println("${gt1.x}")
+    val a = Abc()
+    data = 50;
+
+    val sv = Visibility()
+    sv.v = 10
+    sv.z = 56
+
+    val sb = SubVisibility()
+    sb.v = 10
+    sb.z = 10
 }
+
+class SubVisibility : Visibility() {
+    fun sb() {
+        y = 25
+    }
+}
+
