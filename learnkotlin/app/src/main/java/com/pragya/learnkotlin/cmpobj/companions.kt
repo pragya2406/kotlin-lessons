b678a5f935b4f2d2bf8f30caef174104efc03dd9
pragya2406 <pragyat1920@gmail.com>
2021-10-01 16:52:13 +0530

    companion objects

---
+++ learnkotlin/app/src/main/java/com/pragya/learnkotlin/cmpobj/companions.kt
@@ -0,0 +1,41 @@
+package com.pragya.learnkotlin.cmpobj
+
+class DbConnector {
+
+    private val nm = "nnn"
+    val dbNm = "nnn"
+
+    companion object {
+        val url = "http://abc.com"
+        val userName = "acc"
+        val PASSWORD = "acbv"
+
+        fun crackPassword() {
+            //nm = 10 private member of class
+            // dbNm = "" nothing is accessible from class
+
+            println("Accessing URL $url")
+        }
+    }
+
+    fun connect() {
+        println("Accessing URL $url")
+
+        crackPassword()
+    }
+}
+
+fun main() {
+    val dc = DbConnector() // not needed
+    dc.connect()
+
+    println("Url - ${DbConnector.url}")
+    println("User Name - ${DbConnector.userName}")
+    println("Password - ${DbConnector.PASSWORD}")
+    DbConnector.crackPassword()
+
+    val cmp = DbConnector
+    cmp.userName
+    cmp.crackPassword()
+    cmp.PASSWORD
+}
