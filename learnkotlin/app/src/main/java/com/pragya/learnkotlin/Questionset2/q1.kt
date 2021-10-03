60a6965ebcef918c0cde8df324bcbbc82c65c690
pragya2406 <pragyat1920@gmail.com>
2021-10-03 23:42:49 +0530

    merge two arrays

---
+++ learnkotlin/app/src/main/java/com/pragya/learnkotlin/Questionset2/q1.kt
@@ -0,0 +1,14 @@
+package com.pragya.learnkotlin.Questionset2
+
+import java.util.*
+private fun mergearr(arr1:IntArray,arr2:IntArray): Unit{
+    println("${(arr1+arr2)}")
+}
+
+fun main(){
+
+    val a = intArrayOf(1,2,3,4,5)
+    val b = intArrayOf(4,5,6,7,8)
+    mergearr(a,b)
+
+}
